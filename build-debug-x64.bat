a7157f840e240dff3a3ac29371976a9436c82c7a
vi3t1 <105091408+vi3t1@users.noreply.github.com>
2023-12-13 13:33:02 +0800

    upload build scripts

---
+++ build-debug-x64.bat
@@ -0,0 +1,16 @@
+echo off
+echo build-ultimate.bat: generating VMProtect Debug x64
+
+SET ROOT_DIR=%~dp0
+
+if exist "%ROOT_DIR%\bin" rmdir /q /s "%ROOT_DIR%\bin"
+if exist "%ROOT_DIR%\tmp" rmdir /q /s "%ROOT_DIR%\tmp"
+
+msbuild vmprotect.sln /p:Configuration=Debug /p:Platform=Win32 /t:VMProtectSDK
+msbuild vmprotect.sln /p:Configuration=Debug /p:Platform=x64 /t:VMProtectSDK
+msbuild vmprotect.sln /p:Configuration=Debug /p:Platform=x64 /t:VMProtectCon
+msbuild vmprotect.sln /p:Configuration=Debug /p:Platform=x64 /t:VMProtect
+
+windeployqt "%ROOT_DIR%\bin\64\Debug\VMProtect.exe"
+
+copy "%ROOT_DIR%\bin\32\Debug\*" "%ROOT_DIR%\bin\64\Debug"
---
+++ build-ultimate-x86.bat
@@ -0,0 +1,17 @@
+echo off
+echo build-ultimate.bat: generating VMProtect Ultimate x86
+
+SET ROOT_DIR=%~dp0
+
+if exist "%ROOT_DIR%\bin" rmdir /q /s "%ROOT_DIR%\bin"
+if exist "%ROOT_DIR%\tmp" rmdir /q /s "%ROOT_DIR%\tmp"
+
+msbuild vmprotect.sln /p:Configuration=Ultimate /p:Platform=Win32 /t:VMProtectSDK
+msbuild vmprotect.sln /p:Configuration=Ultimate /p:Platform=x64 /t:VMProtectSDK
+msbuild vmprotect.sln /p:Configuration=Ultimate /p:Platform=Win32 /t:VMProtectCon
+msbuild vmprotect.sln /p:Configuration=Ultimate /p:Platform=Win32 /t:VMProtect
+
+windeployqt "%ROOT_DIR%\bin\32\Ultimate\VMProtect.exe"
+
+copy "%ROOT_DIR%\bin\32\Release\*" "%ROOT_DIR%\bin\32\Ultimate"
+copy "%ROOT_DIR%\bin\64\Release\*" "%ROOT_DIR%\bin\32\Ultimate"
